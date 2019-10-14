30684c41aeda0963823ca3a62a3f7b5b88b48d11
liitng3 <898260697@qq.com>
2019-10-14 15:40:30 +0800

    add

---
+++ precache-manifest.044d2cac3f30a01024730e77387fd3df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cce303ce999bd0f124bb5ea194fe4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4197c150cabe20af99da",
+    "url": "/static/css/main.e1b06ec5.chunk.css"
+  },
+  {
+    "revision": "6391ae8687e6b56aac07",
+    "url": "/static/js/2.06adc54f.chunk.js"
+  },
+  {
+    "revision": "4197c150cabe20af99da",
+    "url": "/static/js/main.6b0cf048.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
